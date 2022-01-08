87f70fb0153cad9c0138a8f297a19d9c9c3288a7
CFM <cpierrelouis37@gmail.com>
2022-01-08 11:35:08 -0600

    Adding some more questions and cleaning up the json package and nodes

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ var inquirer = require('inquirer');
         //     message: "What's your email?",
         //     name: "email"
         // },
-        {
+        { type: "input",
+            message: " What is team member's name?",
+        name: "membername"
+    },{
             type: "list",
-            message: "Which best describes your role? ",
+            message: "Which best describes the team member's role? ",
             name: "role",
             choices: [
                 "Manager",
@@ -19,15 +22,32 @@ var inquirer = require('inquirer');
                 "Engineer", 
                 "Intern" 
             ]
-        }]
+        },
+        
+        {type: "input",
+        message: "What is team member's id?",
+        name: "id"
+    },
+    { type:"input",
+        message: "What is the team member's email address?",
+        name: "email"
+    },
+            {
+            type: "list",
+            message: "Would you like to add more team members?",
+            choices: [
+                "yes",
+                "no"
+            ],
+            name: "moreMembers"}]
 function startWorking(){
     console.log("Build the team")
     inquirer.prompt(questions).then((answers)=>{
         console.log(answers)
         if(answers.role === "Manager"){
             // run another inquirer prompt that asks the manager questions
-        } else if(answers.role==="Engineer")
-    })
+    //     } else if(answers.role==="Engineer")
+    // })
 }
 startWorking()
         // questions()  Note you cannnot call an array. Only a function.
